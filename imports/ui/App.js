65e3e808faa6f548be7dcc42c8370fe4dd307e3f
Luke Romberg <Lukerromberg@gmail.com>
2018-01-26 15:55:48 -0700

    fixed App.js, back to const

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
 import Header from './Components/Header';
 import Main from './Components/Main';
 
 const App = ({data}) => {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      value: ''
-    };
-  }
-  if(data.loading) return null;
+if(data.loading) return null;
 return (
   <div>
     <Header />
     <Main />
-    <GamerLogin stat={this.state}/>
     <ul>
       {data.resolutions.map(resolution => (
         <li key={resolution._id}>
@@ -28,7 +18,7 @@ return (
       ))}
     </ul>
 </div>
-)};
+)}
 
 const hiQuery = gql `
   {
--- imports/ui/Components/GamerLogin.js
+++ imports/ui/Components/GamerLogin.js
@@ -2,18 +2,6 @@ import React from 'react';
 import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 class GamerLogin extends React.Component {
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-    return null;
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
   render() {
     return (
       <form>
